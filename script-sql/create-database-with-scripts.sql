9d59451ac4a9c8324202dcc98725ef843b4c7083
owenwilson <owenwilson_m_f@hotmail.com>
2026-09-02 00:57:32 -0400

    feat: add sample script sql

---
+++ script-sql/create-database-with-scripts.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE sqlDatabaseApp;
+
+USE sqlDatabaseApp;
+GO
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(255) UNIQUE NOT NULL,
+    RegistrationDate DATETIME DEFAULT GETDATE()
+);
+GO
+
+CREATE TABLE FacebookAccount (
+    FacebookID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL,
+    FacebookAccountID NVARCHAR(100) NOT NULL,
+    ProfileURL NVARCHAR(255),
+    ConnectedDate DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+GO
+
+--DROP TABLE dbo.FacebookAccount;
+--DROP TABLE dbo.Users;
+
